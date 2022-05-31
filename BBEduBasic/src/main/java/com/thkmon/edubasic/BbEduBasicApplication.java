77b483b0fa320c6cc60fd2a58f4caa8db131b33d
thkmon <thkmon@gmail.com>
2022-06-01 04:16:19 +0900

    Initial Commit

---
+++ BBEduBasic/src/main/java/com/thkmon/edubasic/BbEduBasicApplication.java
@@ -0,0 +1,13 @@
+package com.thkmon.edubasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbEduBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbEduBasicApplication.class, args);
+	}
+
+}
---
+++ BBEduBasic/src/test/java/com/thkmon/edubasic/BbEduBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thkmon.edubasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbEduBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
